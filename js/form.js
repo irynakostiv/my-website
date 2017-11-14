869be2ac3c39cfe897446be3cb0e6b5bcfd25289
Iryna Kostiv <kostivira@ukr.net>
2017-11-14 15:30:07 +0000

    add js

---
+++ js/form.js
@@ -0,0 +1,26 @@
+/*global $*/
+$('form input[type="submit"]').click(sendForm);
+function sendForm(e) {
+    const form = $('form')[0];
+        if (!form.checkValidity()) {
+            return;
+        }
+    e.preventDefault();
+    $.ajax({
+        url: "https://formspree.io/kostivira@ukr.net", 
+        method: "POST",
+        data: {
+            name: $('#name').val(),
+            mail: $('#mail').val(),
+            comment: $('#comment').val()
+        },
+        dataType: "json"
+    })
+    .done(function() {
+        $('form')[0].reset();
+        $('#msg').html('Дякуємо за Ваш відгук!');
+    })
+    .fail(function() {
+        $('#msg').html('Вибачте, сталась помилка!');
+    });
+}
